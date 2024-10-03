79d8f05a239e40cfef0abda08394133e46e6370e
Mazharul Islam Tamim <mazharulislamtamim28@gmail.com>
2024-10-03 13:01:36 +0600

    update loggin

--- real_estate_rest/settings/base.py
+++ real_estate_rest/settings/base.py
@@ -122,41 +122,34 @@ logger = logging.getLogger(__name__)
 
 LOG_LEVEL = "INFO"
 
-logging.config.dictConfig({
-    "version": 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        "console":{
-            "format": "%(asctime)s %(name)-12s %(lavelname)-8s  %(message)s",
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "console": {
+                "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+            },
+            "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+            "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
         },
-        "file": {
-            "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message"
-        },
-        "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
-        "handlers":{
+        "handlers": {
             "console": {
                 "class": "logging.StreamHandler",
-                "formatter": "console"
+                "formatter": "console",
             },
             "file": {
+                "level": "INFO",
                 "class": "logging.FileHandler",
-                "level": "INFO",
                 "formatter": "file",
-                "filename": "logs/estate_rest.log",
+                "filename": "logs/real_estate.log",
             },
-            "django.server":  DEFAULT_LOGGING["handlers"]["django.server"],
-        }, 
-        "loggers":{
-            "": {
-                "level": "INFO",
-                "handlers": ["console", "file"],
-                "propagate": False
-            },
-            "apps": {
-                "level": "INFO",
-                "handlers": ["console", "file"],
-                "propagate": False
-            }
-        }
-}
-})
+            "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+        },
+        "loggers": {
+            "": {"level": "INFO", "handlers": ["console", "file"], "propagate": False},
+            "apps": {"level": "INFO", "handlers": ["console"], "propagate": False},
+            "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
+        },
+    }
+)
